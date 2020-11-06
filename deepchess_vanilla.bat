b478f118c3f91e68b67d7278689359cf57c614fd
Sean Hastings <sean_hastings@brown.edu>
2020-11-06 15:26:21 -0500

    siamese and vanilla training pipeline update

--- deepchess_vanilla.bat
+++ deepchess_vanilla.bat
@@ -1,4 +1,3 @@
 python parse_data.py --dataset ccrl
-python train_autoencoder.py --dataset ccrl --batch_size 4096 --decay 0.1 --id overnight --epochs 3 --dropout 0.05
-python featurize.py --dataset ccrl --model_id overnight
-python train_siamese.py --dataset ccrl --batch_size 4096 --epochs 10000 --id overnight --dropout 0.05
+python train_autoencoder.py --dataset ccrl --batch_size 4096 --decay 0.1 --id overnight
+python train_siamese.py --dataset ccrl --batch_size 4096 --epochs 1000 --id overnight --ae_source overnight
--- train_siamese.py
+++ train_siamese.py
@@ -21,14 +21,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Training a siamese model')
     parser.add_argument('--dataset', type=str, default='ccrl', metavar='N',
                         help='name of the dataset to parse (default: ccrl)')
+    parser.add_argument('--ae_source', type=str, default='', metavar='N',
+                        help='The id of an autoencoder to initialize weights from (default: "")')
     args, _ = parser.parse_known_args()
+    args.ae_source = None if len(args.ae_source) == 0 else args.ae_source
 
     train_set = SiameseSet(args.dataset, 'train', 1000000, False)
     test_set  = SiameseSet(args.dataset, 'test', 50000, False)
 
     model = Siamese
     model_shape  = (400, 200, 100)
-    model_kwargs = {'hidden_size': model_shape}
+    model_kwargs = {'hidden_size': model_shape, 'AE_init': args.ae_source}
 
     test_functions = {'accuracy': get_acc}
 
